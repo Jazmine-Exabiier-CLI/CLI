a189cbd3d87563eff206dc302628037dfa6e3ab1
JazMac <jazmine.n.hudson@gmail.com>
2022-10-27 10:10:36 -0500

    ConstactsList created

--- src/CLI/CliMain.java
+++ src/CLI/CliMain.java
@@ -2,8 +2,11 @@ package CLI;
 
 public class CliMain {
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
+        ContactsList contact1 = new ContactsList("Jaz", "9032935456");
+        System.out.println(contact1.getName());
+        System.out.println(contact1.getNumber());
     }
 
 }
--- src/CLI/ContactsList.java
+++ src/CLI/ContactsList.java
@@ -1,12 +1,39 @@
 package CLI;
 
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class ContactsList {
 
-    private ArrayList<String> name;
+//    private ArrayList<String> name;
+    private String name;
     private String number;
+    //=========================GETTING AND SETTING NAME=============================
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    //=========================GETTING AND SETTING NUMBER=============================
+    public String getNumber() {
+        return number;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    //===========================toString Method====================
+    public String toString(){
+        return name;
+    }
+
+    public ContactsList(){}
+
+    public ContactsList(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+
 
 
 }
