e97584efbcaa788da797e46598b064390078a4e0
Exabiier <exabiier.bars@gmail.com>
2022-10-27 09:57:35 -0500

    Created .txt file

---
+++ src/CLI/CliMain.java
@@ -0,0 +1,9 @@
+package CLI;
+
+public class CliMain {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/CLI/ContactsList.java
@@ -0,0 +1,12 @@
+package CLI;
+
+
+import java.util.ArrayList;
+
+public class ContactsList {
+
+    private ArrayList<String> name;
+    private String number;
+
+
+}
